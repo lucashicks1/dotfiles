b1f41d70ce6fb6010bbfbcb378031886ec33ced4
lucashicks1 <lucasjhicks1@gmail.com>
2023-10-31 14:53:59 +1000

    Updates lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -2,3 +2,15 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { name = "black" }, }
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "ruff", filetypes = { "python" } } }
+
+lvim.builtin.treesitter.ensure_installed = {
+  "python",
+}
+lvim.format_on_save.enabled = true
+lvim.format_on_save.pattern = { "*.py" }
