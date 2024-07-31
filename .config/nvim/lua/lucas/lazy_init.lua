cda2c7bce505d5a62253c4fdc727a400037f839f
lucashicks1 <lucasjhicks1@gmail.com>
2024-07-31 23:32:33 +1000

    new: Updates nvim config, uses lazy now with proper lsp setup

---
+++ .config/nvim/lua/lucas/lazy_init.lua
@@ -0,0 +1,23 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("lazy").setup({
+    spec = "lucas.lazy",
+    change_detection = { notify = false }
+})
