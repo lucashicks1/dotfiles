956c417ab2747d5283371e255d9646d3b46ceb77
= <lucas@mac.lan>
2025-07-25 15:17:18 +1000

    kickstart nvim

--- .config/nvim/lua/lucas/init.lua
+++
@@ -1,9 +0,0 @@
-require("lucas.set")
-require("lucas.remap")
-require("lucas.lazy_init")
-
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
-
-vim.cmd.colorscheme("catppuccin")
