cb1522d1d8b15f4fcf5806dd3fb03b2b44f50975
Lucas Hicks <lucasjhicks1@gmail.com>
2025-08-14 21:49:59 +1000

    new: Adds '\' command for opening terminal.

--- .config/nvim/lua/custom/plugins/toggleterm.lua
+++ .config/nvim/lua/custom/plugins/toggleterm.lua
@@ -3,5 +3,8 @@ return {
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {},
+    config = {
+      vim.keymap.set('n', '\\', '<CMD>ToggleTerm direction=float<CR>', { desc = 'Open parent directory' }),
+    },
   },
 }
