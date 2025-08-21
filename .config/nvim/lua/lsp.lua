f214f51308ae93968bafbf1d8fbc3dc8538c04d1
Lucas Hicks <lucasjhicks1@gmail.com>
2025-08-21 10:11:16 +1000

    fi: removes csharp lsp

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -10,7 +10,6 @@ local servers = {
   rust_analyzer = {},
   pyright = {},
   jdtls = {},
-  csharp_ls = {},
   -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
   --
   -- Some languages (like typescript) have entire language plugins that can be useful:
