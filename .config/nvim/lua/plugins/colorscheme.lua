62cb7daf4cc318928f1232f731de5a9e6afccd5a
Lucas Hicks <lucasjhicks1@gmail.com>
2025-11-02 19:18:16 +1000

    new: Fixes treesitter so now I actually have proper highlighting.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,17 +4,35 @@ return {
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     require('catppuccin').setup {
-      styles = {
-        comments = { 'italic' }, -- Disable italics in comments
-      },
       flavour = 'mocha',
+      background = { -- :h background
+        light = 'latte',
+        dark = 'mocha',
+      },
+      float = {
+        transparent = false, -- enable transparent floating windows
+        solid = false, -- use solid styling for floating windows, see |winborder|
+      },
+      auto_integrations = true,
       integrations = {
         cmp = true,
         gitsigns = true,
         nvimtree = true,
+        notify = true,
+        dap = true,
+        blink_cmp = {
+          enabled = true,
+          style = 'bordered',
+        },
         mini = {
           enabled = true,
         },
+        barbar = true,
+        treesitter_context = true,
+        telescope = {
+          enabled = true,
+        },
+        lsp_trouble = true,
       },
     }
 
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,40 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  opt = {
-    ensure_installed = {
-      'bash',
-      'c',
-      'diff',
-      'lua',
-      'luadoc',
-      'markdown',
-      'vim',
-      'vimdoc',
-      'python',
-      'go',
-      'yaml',
-      'json',
-      'rust',
-    },
-    sync_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        ensure_installed = {
+          'bash',
+          'c',
+          'diff',
+          'lua',
+          'luadoc',
+          'markdown',
+          'vim',
+          'vimdoc',
+          'python',
+          'go',
+          'yaml',
+          'json',
+          'rust',
+        },
+        auto_install = true,
+        sync_install = false,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+      }
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require('treesitter-context').setup {
+        enable = true,
+      }
+    end,
   },
 }
