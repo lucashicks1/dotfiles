ef1a6520e353facb38b51c06aaa3f68aa8a417d0
Lucas Hicks <lucasjhicks1@gmail.com>
2026-05-05 21:40:22 +1000

    fix: no more auto format on save

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -5,10 +5,10 @@ return {
   opts = {
     notify_on_error = true,
     notify_no_formatters = true,
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = 'fallback',
-    },
+    -- format_on_save = {
+    --   timeout_ms = 500,
+    --   lsp_format = 'fallback',
+    -- },
     formatters_by_ft = {
       -- Prog. Languages
       bash = { 'shfmt' },
